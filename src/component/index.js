d7d0630473d4b0713ceafe4d0a854aa990773e1f
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2022-03-30 23:14:50 +0530

    Feat-Routes and few basic component
    Added Routes
    created basic pages and components file link together for a initial setup of the project

---
+++ src/component/index.js
@@ -0,0 +1,6 @@
+import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer/Footer";
+export {
+    Navbar,
+    Footer
+}
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Home from "./Home/Home.js";
+import PlayListPage from './PlayListPage/PlayListPage.js';
+import VideoListingPage from "./VideoListPage/VideoListingPage.js";
+export {
+    Home,
+    PlayListPage,
+    VideoListingPage
+}
