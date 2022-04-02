96f4946b4b89a892bb05315081858c2dcd24c889
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2022-04-03 02:29:22 +0530

    Added create playlist functionality

--- src/component/PlaylistModal/PlaylistModal.js
+++ src/component/PlaylistModal/PlaylistModal.js
@@ -4,7 +4,14 @@ import { IoCloseCircle } from "react-icons/io5";
 import { useUserDetail } from "../../context/userDetail-context";
 function PlaylistModal() {
   const [inputToggle, setInputToggle] = useState(false);
-  const { modalDisplay, setModalDisplay } = useUserDetail();
+  const [inputValue , setInputValue] = useState("");
+  const { modalDisplay, setModalDisplay, userDetailState, userDetailDispatch } = useUserDetail();
+  const {playlist} = userDetailState;
+  const createPlaylistHandler = () => {
+    setInputToggle((prev) => !prev);
+    inputValue && userDetailDispatch({ type:"CREATE_PLAYLIST", payload:inputValue})
+  }
+  console.log(playlist)
   return (
     <div
       className={`${
@@ -18,10 +25,20 @@ function PlaylistModal() {
             <IoCloseCircle className="lightcolor txt-lg poitner-cursor" />
           </div>
         </div>
-        {inputToggle && <input type="text" />}
+        {
+           playlist.length && playlist.map((item) => {
+                return(
+                    <>
+                    <input type="checkbox" />
+                    <h1 className="lightcolor">{item.playlistname}</h1>
+                    </>
+                )
+            })
+        }
+        {inputToggle && <input type="text"  onChange={(e) => setInputValue(e.target.value)}/>}
         <button
           onClick={() => {
-            setInputToggle((prev) => !prev);
+            createPlaylistHandler()
           }}
         >
           Add new playlist
--- src/reducer/userDetailReducer.js
+++ src/reducer/userDetailReducer.js
@@ -1,4 +1,8 @@
-const userDetailReducer = () => {
-
+import { v4 as uuid } from "uuid";
+const userDetailReducer = (state , action) => {
+    switch(action.type){
+        case "CREATE_PLAYLIST" :
+            return{...state,playlist:[...state.playlist,{playlistname:action.payload,playlistId:uuid()}]}
+    }
 }
 export {userDetailReducer};
