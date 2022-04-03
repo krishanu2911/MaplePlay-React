680a671dfb4c310e5c14845f3134e51f635ae7cf
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2022-04-03 16:59:00 +0530

    Added feature adding new playlist

--- src/pages/PlayListPage/PlayListPage.js
+++ src/pages/PlayListPage/PlayListPage.js
@@ -1,32 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
 import { useUserDetail } from "../../context/userDetail-context";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import PlayListCard from "./PlayListCard";
 function PlayListPage() {
-    const videoListNavigate = useNavigate()
-    const {userDetailState} = useUserDetail();
-    const {playlist} = userDetailState;
-    return (
-        <div className='padding-m'>
-            <h1 className='lightcolor '>Playlists</h1>
-            <div className='maple-flex-xy-center'>
-            
-            {
-               playlist.length ? playlist.map((item) => {
-                   return<PlayListCard listDetail={item} key={item.playlistId}/>
-               })
-               :
-               <button 
-               onClick={() => {
-                videoListNavigate("/videolist")
-               }}
-               className='filterBtn padding-m txt-xl boldest-font'
-               
-               >Create Playlist</button>
-            }
-        </div>
+  const videoListNavigate = useNavigate();
+  const { userDetailState, userDetailDispatch } = useUserDetail();
+  const { playlist } = userDetailState;
+  const [newPlaylistName, setNewPlayListName] = useState("");
+  return (
+    <div className="padding-m">
+      <div className="maple-flex gap-m playlist-header">
+        <h1 className="lightcolor ">All Playlists</h1>
+        <div className="maple-flex gap-m">
+           <input
+          type="text"
+          onChange={(e) => setNewPlayListName(e.target.value)}
+        />
+        <button
+        className="filterBtn padding-sm txt-m boldest-font"
+          onClick={() => {
+            newPlaylistName &&
+              userDetailDispatch({
+                type: "CREATE_PLAYLIST",
+                payload: newPlaylistName,
+              });
+          }}
+        >
+          Add new playlist
+        </button> 
         </div>
         
-    )
+      </div>
+
+      <div className="maple-flex-xy-center">
+        {playlist.length ? (
+          playlist.map((item) => {
+            return <PlayListCard listDetail={item} key={item.playlistId} />;
+          })
+        ) : (
+          <button
+            onClick={() => {
+              videoListNavigate("/videolist");
+            }}
+            className="filterBtn padding-m txt-xl boldest-font"
+          >
+            Go to Explore
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
 export default PlayListPage;
