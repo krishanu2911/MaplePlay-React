ae89f153781cb47f1a295904c252f41b42286aea
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2022-04-07 12:36:29 +0530

    Update Mockbee db

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -11,52 +11,52 @@ export const videos = [
     category:"Stoicism",
     thumbnail:"https://img.youtube.com/vi/EFkyxzJtiv4/sddefault.jpg",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "Stoicism helps us steer through past and present storms into calmer and more peaceful waters. And if our ship sinks and we all drown, we can take peace in the fact that we lived a good life, albeit not as long as we had hoped. Because remember, everything has an end.",
+    creator: "Aperture",
   },
   {
     _id: "2bguEiUgDA4",
     title: "BE UNSHAKEABLE - The Ultimate Stoic Quotes",
-    creator: "Sentdex",
+    creator: "RedFrost",
     category:"Stoicism",
     thumbnail:"https://img.youtube.com/vi/2bguEiUgDA4/sddefault.jpg",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "Powerful wisdom from Marcus Aurelius, Seneca and Epictetus",
   },
   {
     _id: "yH86jaBQ0F4",
     title: "A Lesson From Socrates That Will Change The Way You Think",
-    creator: "Sentdex",
+    creator: "Robot Banana",
     category:"Socrates",
     thumbnail:"https://img.youtube.com/vi/yH86jaBQ0F4/sddefault.jpg",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "A classical Greek philosopher credited as one of the founders of Western philosophy, and as being the first moral philosopher, of the Western ethical tradition of thought.",
   },
   {
     _id: "Y4y2e4rUxfo",
     title: "9 Life Lessons From Socrates",
-    creator: "Sentdex",
+    creator: "The School of Life",
     category:"Socrates",
     thumbnail:"https://img.youtube.com/vi/Y4y2e4rUxfo/sddefault.jpg",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "In this video we will be talking about 9 Life Lessons From Socrates. Socrates is sometimes considered as the founder of the Western philosophy and his work reflects the philosophy of Skepticism.",
   },
   {
     _id: "VDiyQub6vpw",
     title: "PHILOSOPHY - Plato",
-    creator: "Sentdex",
+    creator: "The School of Life",
     category:"Plato",
     thumbnail:"https://img.youtube.com/vi/VDiyQub6vpw/sddefault.jpg",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "Plato was one of the world's earliest and possibly greatest philosophers. He matters because of his devotion to making humanity more fulfilled.",
   },
   {
     _id: "2S2nl8U8Vuk",
     title: "Friedrich Nietzsche - How To Achieve Self Mastery (Existentialism)",
-    creator: "Sentdex",
+    creator: "Philosophies for Life",
     category:"Friedrich Nietzsche",
     thumbnail:"https://img.youtube.com/vi/2S2nl8U8Vuk/sddefault.jpg",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "In this video we bring you how to achieve self mastery from the philosophy of Friedrich Nietzsche. Friedrich Nietzsche believed that the fundamental drive of all living things is the so-called Will to Power, which is often seen as the impulse to overcome one’s environment, the impulse to grow, expand, and explore.",
   },
 ];
