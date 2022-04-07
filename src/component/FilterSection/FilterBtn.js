4b13bcc104bef77661a4f7260c0d651fc1c709bc
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2022-04-07 11:04:22 +0530

    Fix active and unactive filter btn

--- src/component/FilterSection/FilterBtn.js
+++ src/component/FilterSection/FilterBtn.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
 import { useFilter } from "../../context/filter-context";
 
-function FilterBtn({categoryName}) {
-    const {filterDispatch} = useFilter();
-    return (
-        <button className='filterBtn txt-lg boldest-font padding-sm' onClick={() => {
-            filterDispatch({ type: "SET_CATEGORY" , payload: categoryName})
-        }}>{categoryName}</button>
-    )
+function FilterBtn({ categoryName }) {
+  const { filterDispatch, filterState } = useFilter();
+  const { filterSelect } = filterState;
+  return (
+    <button
+      className={`${filterSelect === categoryName ? "filterBtn" : "unactiveBtn"} txt-lg boldest-font padding-sm`}
+      onClick={() => {
+          console.log(filterState )
+        filterDispatch({ type: "SET_CATEGORY", payload: categoryName });
+      }}
+    >
+      {categoryName}
+    </button>
+  );
 }
 export default FilterBtn;
